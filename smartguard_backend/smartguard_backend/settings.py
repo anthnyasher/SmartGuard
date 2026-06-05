a4590fb0b350550590d1bc63e7969be0dff75160
SmartGuard Dev <smartguard@example.com>
2026-06-05 19:14:43 +0800

    Remove DRF session auth

--- smartguard_backend/smartguard_backend/settings.py
+++ smartguard_backend/smartguard_backend/settings.py
@@ -140,7 +140,6 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
