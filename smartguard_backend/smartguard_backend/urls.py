a8b784c5708287e5251a92f2caa5683d0ed3afc0
SmartGuard Dev <smartguard@example.com>
2026-06-05 18:13:21 +0800

    Update admin url

--- smartguard_backend/smartguard_backend/urls.py
+++ smartguard_backend/smartguard_backend/urls.py
@@ -6,7 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api-admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path('api/', include('accounts.urls')),
     path('api/', include('cameras.urls')),
